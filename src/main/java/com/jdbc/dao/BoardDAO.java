c67dc56b76b2243a4c71237fe84d85b7e6029c48
Miny1110 <wjdalswjd453@naver.com>
2022-08-30 16:08:22 +0900

    0830

--- src/main/java/com/jdbc/dao/BoardDAO.java
+++ src/main/java/com/jdbc/dao/BoardDAO.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -26,331 +28,84 @@ public class BoardDAO {
 		
 		int maxNum = 0;
 		
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql;
-		
-		try {
-			
-			sql = "select nvl(max(num),0) from board";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				maxNum = rs.getInt(1);
-			}
-			
-			rs.close();
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		maxNum = sessionTemplate.selectOne("com.boardMapper.maxNum");
 		
 		return maxNum;
 		
 	}
 	
 	//입력
-	public int insertData(BoardDTO dto) {
+	public void insertData(BoardDTO dto) {
 		
-		int result = 0;
-		PreparedStatement pstmt = null;
-		String sql;
-		
-		try {
-			
-			sql = "insert into board(num,name,pwd,email,subject,";
-			sql+= "content,ipAddr,hitCount,created) ";
-			sql+= "values (?,?,?,?,?,?,?,0,sysdate)";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1, dto.getNum());
-			pstmt.setString(2, dto.getName());
-			pstmt.setString(3, dto.getPwd());
-			pstmt.setString(4, dto.getEmail());
-			pstmt.setString(5, dto.getSubject());
-			pstmt.setString(6, dto.getContent());
-			pstmt.setString(7, dto.getIpAddr());
-			
-			result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		
-		return result;
+		sessionTemplate.insert("com.boardMapper.insertData",dto);
 		
 	}
 	
-	
+	//전체데이터 가져오기
 	public List<BoardDTO> getLists(int start,int end,String searchKey,String searchValue){
 		
-		List<BoardDTO> lists = new ArrayList<BoardDTO>();
+		Map<String, Object> params = new HashMap<String, Object>();
 		
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql;
+		params.put("start", start);
+		params.put("end", end);
+		params.put("searchKey", searchKey);
+		params.put("searchValue", searchValue);
 		
-		try {
-			
-			searchValue = "%" + searchValue + "%";
-			
-			sql = "select * from (";
-			sql+= "select rownum rnum,data.* from(";
-			sql+= "select num,name,subject,hitCount,";
-			sql+= "to_char(created,'YYYY-MM-DD') created ";
-			//sql+= "from board order by num desc) data ) ";
-			sql+= "from board where " + searchKey + " like ? order by num desc) data ) ";
-			sql+= "where rnum>=? and rnum<=?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, searchValue);
-			pstmt.setInt(2, start);
-			pstmt.setInt(3, end);
-			
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
-				
-				BoardDTO dto = new BoardDTO();
-				
-				dto.setNum(rs.getInt("num"));
-				dto.setName(rs.getString("name"));
-				dto.setSubject(rs.getString("subject"));
-				dto.setHitCount(rs.getInt("hitCount"));
-				dto.setCreated(rs.getString("created"));
-				
-				lists.add(dto);
-				
-			}
-			
-			rs.close();
-			pstmt.close();
-			
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+		List<BoardDTO> lists = sessionTemplate.selectList("com.boardMapper.getLists",params);
 		
 		return lists;
+		
 	}
 	
-	
+	//전체데이터의 개수
 	public int getDataCount(String searchKey,String searchValue) {
 		
 		int dataCount = 0;
 		
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql;
+		Map<String, Object> params = new HashMap<String, Object>();
 		
-		try {
-			
-			searchValue = "%" + searchValue + "%";
-			
-			sql = "select nvl(count(*),0) from board ";
-			sql+= "where " + searchKey + " like ?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, searchValue);
-			
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				dataCount = rs.getInt(1);
-			}
-			
-			rs.close();
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+		params.put("searchKey", searchKey);
+		params.put("searchValue", searchValue);
+		
+		dataCount = sessionTemplate.selectOne("com.boardMapper.getDataCount",params);
 		
 		return dataCount;
 		
 	}
 	
 
+	//하나의 데이터 읽어오기
 	public BoardDTO getReadData(int num) {
 		
-		BoardDTO dto = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql;
-		
-		try {
-			
-			sql = "select num,name,pwd,email,subject,content,ipAddr,hitCount,created ";
-			sql+= "from board where num=?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1, num);
-			
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				
-				dto = new BoardDTO();
-				
-				dto.setNum(rs.getInt("num"));
-				dto.setName(rs.getString("name"));
-				dto.setPwd(rs.getString("pwd"));
-				dto.setEmail(rs.getString("email"));
-				dto.setSubject(rs.getString("subject"));
-				dto.setContent(rs.getString("content"));
-				dto.setIpAddr(rs.getString("ipAddr"));
-				dto.setHitCount(rs.getInt("hitCount"));
-				dto.setCreated(rs.getString("created"));
-				
-			}
-					
-			rs.close();
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		
-		return dto;
+		BoardDTO dto = sessionTemplate.selectOne("com.boardMapper.getReadData",num);
 
-	}
-	
-	
-	public int updateHitCount(int num) {
-		
-		int result = 0;
-		
-		PreparedStatement pstmt = null;
-		String sql;
-		
-		try {
-			
-			sql = "update board set hitCount = hitCount + 1 where num=?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1, num);
-			
-			result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		
-		return result;
+		return dto;
 		
 	}
 	
 	
-	public int updatedData(BoardDTO dto) {
+	//조회수 증가
+	public void updateHitCount(int num) {
 		
-		int result = 0;
-		PreparedStatement pstmt = null;
-		String sql;
-		
-		try {
-			
-			sql = "update board set name=?,pwd=?,email=?,subject=?,content=? ";
-			sql+= "where num=?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, dto.getName());
-			pstmt.setString(2, dto.getPwd());
-			pstmt.setString(3, dto.getEmail());
-			pstmt.setString(4, dto.getSubject());
-			pstmt.setString(5, dto.getContent());
-			pstmt.setInt(6, dto.getNum());
-			
-			result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		
-		return result;
-		
+		sessionTemplate.update("com.boardMapper.updateHitCount",num);
 		
 	}
 	
 	
-	public int deleteData(int num) {
+	//수정
+	public void updatedData(BoardDTO dto) {
 		
-		int result = 0;
-		PreparedStatement pstmt = null;
-		String sql;
-		
-		try {
-			
-			sql = "delete board where num=?";
-			
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1, num);
-			
-			result = pstmt.executeUpdate();
-			
-			pstmt.close();
-			
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
-		
-		return result;
+		sessionTemplate.update("com.boardMapper.updateData",dto);
 		
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//삭제
+	public void deleteData(int num) {
+		
+		sessionTemplate.delete("com.boardMapper.deleteData",num);
+		
+	}
 	
 	
 }
